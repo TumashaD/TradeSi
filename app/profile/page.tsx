3dac17891ad6d3644d63bd8dfcfebd2de5537188
Donald Aadithiyan <donaldaadithiyan@gmail.com>
2024-10-17 12:16:36 +0530

    ./

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,7 +9,7 @@ const ProfilePage = () => {
       {/* Top bar container with BackButton on the left and Profile text on the right */}
       <div className="flex w-full items-center px-8 py-4">
         <BackButton size={"sm"} route="/" /> 
-        <h1 className="text-2xl font-bold ml-3">Profile</h1> 
+        <h1 className="text-2xl font-bold ml-8">Profile</h1> 
       </div>
 
       <Toaster /> {/* Toaster that shows notifications */}
