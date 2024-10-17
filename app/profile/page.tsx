3bbb4781e38464fe07449659b18c9ac0f7c0aedb
Donald Aadithiyan <donaldaadithiyan@gmail.com>
2024-10-17 11:21:30 +0530

    Profile UI Tested and done

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { ProfileForm } from "./ProfileForm";
 import { Toaster } from "react-hot-toast"; // Import the Toaster component
+import BackButton from "@/components/admin/back-button";
 
 const ProfilePage = () => {
   return (
-    <div className="flex flex-col h-screen p-4">
-      <Toaster/> {/* Toaster that shows notifications */}
-      <h1 className="text-2xl font-bold mb-4">Profile</h1>
-      <div className="flex-grow">
+    <div className="w-full h-screen flex flex-col">
+      {/* Top bar container with BackButton on the left and Profile text on the right */}
+      <div className="flex w-full items-center px-8 py-4">
+        <BackButton size={"sm"} route="/" /> 
+        <h1 className="text-2xl font-bold ml-3">Profile</h1> 
+      </div>
+
+      <Toaster /> {/* Toaster that shows notifications */}
+      
+      {/* Main content container */}
+      <div className="flex-grow px-8">
         <ProfileForm />
       </div>
     </div>
