fc72eba88eaa3d99c62a0617f9b6a6ff61729717
Isurika Gunasekara <isurikauma@gmail.com>
2024-10-26 23:59:55 +0530

    load products done

--- lib/services.ts
+++ lib/services.ts
@@ -116,10 +116,12 @@ export async function getProduct(id: string): Promise<Product | null> {
  *
  * @export
  * @param {string} [category]
+ * @param {string} [query]
  * @return {Promise<Product[]>}
  */
 export async function getProducts(
-    category?: string
+    category?: string,
+    params?: string
 ): Promise<Product[]> {
     // For enhanced security, the verifySession function can be used to authenticate the user.
     // While middleware is a viable option, verifySession can also be directly utilized within services.
@@ -134,14 +136,12 @@ export async function getProducts(
             const [rows] = await connection.query<any>(
                 "call viewCategoryProducts(?)", [category]
             );
-            const data = JSON.parse(JSON.stringify(rows));
+            const data = JSON.parse(JSON.stringify(rows[0]));
             return data;
         } else {
             const [rows] = await connection.query<any>(
                 `select * from TradeSi.AllProducts`);
             const data = JSON.parse(JSON.stringify(rows));
-            console.log("all products");
-            console.log(data);
             return data;
         }
     } catch (error) {
